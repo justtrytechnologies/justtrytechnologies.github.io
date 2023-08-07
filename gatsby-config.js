0e4aaef7e9378c95b0435702297f67be1b807f6f
vijay07 <vijay.flipflop@gmail.com>
2023-08-07 20:43:17 +0530

    build error removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/logo-2.png`, // This path is relative to the root of the site.
+        icon: `src/styles/assets/images/logo2.png`, // This path is relative to the root of the site.
       },
     },
     // {
