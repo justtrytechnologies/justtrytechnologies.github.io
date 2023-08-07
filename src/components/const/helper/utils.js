1f22be6adc3ba98814c3ddfe6629ac57a53634fd
vijayflipflop <104342161+vijayflipflop@users.noreply.github.com>
2023-08-07 22:01:49 +0530

    file deleted

--- src/components/const/helper/utils.js
+++
@@ -1,9 +0,0 @@
-import { toast } from "react-toastify"
-
-export const showSuccssMsg = (msg) => {
-    toast.success(msg, { autoClose: 2000 })
-};
-
-export const showErrMsg = (msg) => {
-    toast.error(msg, { autoClose: 2000 });
-};
